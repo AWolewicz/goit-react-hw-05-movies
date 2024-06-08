6685b913bbf63ba37a4e052c8dfd399aa041f268
AWolewicz <122787728+AWolewicz@users.noreply.github.com>
2024-06-08 07:30:19 +0000

    Deploying to gh-pages from @ AWolewicz/goit-react-hw-05-movies@70108d7c8069004c490802a69e258a7273269c23 🚀

---
+++ static/js/668.fce1b7d6.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[668],{668:function(e,n,a){a.r(n),a.d(n,{default:function(){return s}});var r="NotFound_main__G4WZP",t="NotFound_container__gVQ7z",o="NotFound_heading__HdNIO",c=a(184),s=function(){return(0,c.jsx)("main",{className:r,children:(0,c.jsxs)("div",{className:t,children:[(0,c.jsx)("h2",{className:o,children:"Error 404"}),(0,c.jsx)("p",{children:"Page not found"})]})})}}}]);
+//# sourceMappingURL=668.fce1b7d6.chunk.js.map
